c89b9b57ecfd1b83d526e6e20b1c1551e8ed2cca
guilhermefos <guilhermefos.developer@gmail.com>
2016-09-05 20:01:27 -0300

    Service - Working well

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,24 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
-
 @Component({
-
-	selector: 'my-app',
-	template: `
-  <h1>{{title}}</h1>
-  <h2>My Heroes</h2>
-  <ul class="heroes">
-    <li *ngFor="let hero of heroes"
-      [class.selected]="hero === selectedHero"
-      (click)="onSelect(hero)">
-      <span class="badge">{{hero.id}}</span> {{hero.name}}
-    </li>
-  </ul>
-  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-`,
-	styles: [`
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `,
+  styles: [`
     .selected {
       background-color: #CFD8DC !important;
       color: white;
@@ -69,32 +66,18 @@ import { HeroService } from './hero.service';
   `],
   providers: [HeroService]
 })
-export class AppComponent implements OnInit{
-
-  ngOnInit():void{
-    this.getHeroes;
-  }
-
-  constructor(
-    private heroService: HeroService
-    ){ }
-
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  selectedHero: Hero;
   heroes: Hero[];
-
+  selectedHero: Hero;
+  constructor(private heroService: HeroService) { }
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-
-    getHeroes(): void {
-  this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
 }
-
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
